a381a7e04b71021e09a38446614923d19703c7e0
Anna Roses <anna.roses.marti@gmail.com>
2023-10-15 14:21:41 +0200

    Add countNeighbours method to world class

--- app/World/World.js
+++ app/World/World.js
@@ -7,15 +7,41 @@ class World {
     this.cell = [];
   }
 
-  initBoard(length, isAlive, ...element) {
+  initBoard(length, isAlive) {
     for (let indexRow = 0; indexRow < length; indexRow++) {
-      this.cell[indexRow] = element;
+      this.cell[indexRow] = [];
       for (let indexColumn = 0; indexColumn < length; indexColumn++) {
         this.cell[indexRow][indexColumn] = new Cell(isAlive);
       }
     }
 
     return this.cell;
+  }
+
+  countNeighbours(indexRow, indexColumn) {
+    let counter = 0;
+    const length = 3;
+
+    const myCell = this.cell[indexRow][indexColumn];
+    const leftRow = indexRow - 1 === -1 ? 0 : indexRow - 1;
+    const rightRow = indexRow + 1 > length - 1 ? length - 1 : indexRow + 1;
+    const leftColumn = indexColumn - 1 === -1 ? 0 : indexColumn - 1;
+    const rightColumn =
+      indexColumn + 1 > length - 1 ? length - 1 : indexColumn + 1;
+
+    for (let x = leftRow; x <= rightRow; x++) {
+      for (let y = leftColumn; y <= rightColumn; y++) {
+        if (this.cell[x][y].state()) {
+          counter += 1;
+        }
+      }
+    }
+
+    if (myCell.state()) {
+      counter -= 1;
+    }
+
+    myCell.neighbours = counter;
   }
 }
 
